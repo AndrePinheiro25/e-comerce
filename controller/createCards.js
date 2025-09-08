f50cf096ebfa4c645b3d3032673f0bab8693c953
AndrePinheiro25 <andre.gpinheiro25@gmail.com>
2025-09-08 17:25:43 -0300

    atualização de erros

---
+++ controller/createCards.js
@@ -0,0 +1,12 @@
+const cardsSection = document.getElementById("cards");
+
+export function createCards() {
+    for (let i = 0; i < 15; i++) {
+        let card = document.createElement('div');
+        card.className = 'card';
+
+        //O resto do cartão
+
+        cardsSection.appendChild(card);
+    }
+}

5414c8b316158842b3d649cc3a9ccab65d8fa32e
AndrePinheiro25 <andre.gpinheiro25@gmail.com>
2025-09-29 14:20:45 -0300

    melhorias

--- controller/createCards.js
+++ controller/createCards.js
@@ -1,22 +1,25 @@
-// import { bdcards } from "./bdcards.js";
+import { buscarInformacoes } from "../services/cards_services.js";
+
 const cardsSection = document.getElementById("cards");
 
-export function createCards() {
-    for (let i = 0; i < 15; i++) {
+export async function createCards() {
+    let bdCards = await buscarInformacoes();
+    for (let i = 0; i < bdCards.length; i++) {
         let card = document.createElement('div');
         card.className = 'card';
 
         let copa = document.createElement("div");
         copa.className = "copa";
 
-        let img = document.createElement("img");
-        img.className = "logo";
-        img.src = "https://tse3.mm.bing.net/th/id/OIP.PMBIZTQNobBOOYKK3UrLXwHaI4?rs=1&pid=ImgDetMain&o=7&rm=3";
-        copa.appendChild(img);
+        let img_copa = document.createElement('div');
+        img_copa.className = 'img';
+        img_copa.style.backgroundImage = `url(${bdCards[i].logo})`;
+
+        copa.appendChild(img_copa);
 
         let ano = document.createElement("div");
-        ano.className = "ano";
-        ["1", "9", "3", "0"].forEach(num => {
+        ano.className = 'ano';
+        [bdCards[i].ano[0], bdCards[i].ano[1], bdCards[i].ano[2], bdCards[i].ano[3]].forEach(num => {
             let p = document.createElement("p");
             p.textContent = num;
             ano.appendChild(p);
@@ -24,8 +27,6 @@ export function createCards() {
         copa.appendChild(ano);
 
         card.appendChild(copa);
-
-        
 
         let informacoes = document.createElement("div");
         informacoes.className = "informacoes";
@@ -37,19 +38,19 @@ export function createCards() {
         campeao.textContent = 'Campeão';
 
         let p1 = document.createElement("h1")
-        p1.textContent = 'Uruguai';
+        p1.textContent = bdCards[i].campeao;
 
         let vice = document.createElement("h3")
         vice.textContent = 'Vice';
 
         let p2 = document.createElement("h1")
-        p2.textContent = 'Tchecoslováquia';
+        p2.textContent = bdCards[i].viceCampeao;
 
         let sede = document.createElement("h3")
         sede.textContent = 'Sede';
 
         let p3 = document.createElement("h1")
-        p3.textContent = 'Uruguai';
+        p3.textContent = bdCards[i].sede;
 
         dados.appendChild(campeao);
         dados.appendChild(p1);
@@ -60,11 +61,11 @@ export function createCards() {
 
         informacoes.appendChild(dados);
 
-        let artilheiro = document.createElement("img");
-        artilheiro.className = "artilheiro";
-        artilheiro.src = "https://tse2.mm.bing.net/th/id/OIP.j0ageLhdLgvdTCWWykrPWAHaKp?rs=1&pid=ImgDetMain&o=7&rm=3";
+        let img_art = document.createElement('div');
+        img_art.className = 'img';
+        img_art.style.backgroundImage = `url(${bdCards[i].artilheiro})`
 
-        informacoes.appendChild(artilheiro);
+        informacoes.appendChild(img_art);
 
         card.appendChild(informacoes)
 
---
+++ services/cards_services.js
@@ -0,0 +1,27 @@
+const apiUrl = 'http://localhost:3000/copas-do-mundo';
+export async function buscarInformacoes() {
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`Erro na requisição: ${response.status} ${response.statusText}`)
+        }
+        const dados = await response.json();
+
+        console.log('Dados recebidos com sucesso:', dados);
+
+        for (let i = 0; i < dados.length; i++) {
+            for (let j = 0; j < (dados.length - 1); j++) {
+                if (dados[j].ano > dados[j + 1].ano) {
+                    let aux = dados[j];
+                    dados[j] = dados[j + 1];
+                    dados[j + 1] = aux;
+                }
+            }
+        }
+
+        return dados;
+
+    } catch (error) {
+        console.error('falha ao buscar informações:', error);
+    }
+}
